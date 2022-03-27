d33d1fee1016e280e175be2137e2b01dc6577611
Arpita-Jaiswal <ayushipujaa@gmail.com>
2022-03-27 11:53:19 +0530

    Update PackageSerializer to include DocumentSerializer

--- backend/play/serializers.py
+++ backend/play/serializers.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 from .models import Package, Document
 
 
-class PackageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Package
-        fields = ('id', 'path', 'description', 'created_on', 'updated_on')
-
-
 class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
         fields = ('id', 'path', 'content', 'package', 'created_on', 'updated_on', 'base_path')
+
+
+class PackageSerializer(serializers.ModelSerializer):
+    all_documents = DocumentSerializer(source='documents', many=True)
+
+    class Meta:
+        model = Package
+        fields = ('id', 'path', 'description', 'created_on', 'updated_on', 'all_documents')
